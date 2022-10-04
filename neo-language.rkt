d2394438cd0143218e9092c157bf0162e921d160
blade <yiming.liao@cuw.edu>
2022-10-04 10:47:00 -0500

    add boolean
    1 add bool expression in parcer and runner
    2 add ask expression
    3 fix bugs

--- neo-language.rkt
+++ neo-language.rkt
@@ -20,13 +20,23 @@
 (define neo-parser
   (lambda (neo-code)
     (cond
+      ((null? neo-code) '())
       ((number? neo-code) (list 'num-exp neo-code))
       ((symbol? neo-code) (list 'var-exp neo-code))
+      ;(bool op num1 num2) > (bool-exp op (neo-exp) (neo-exp))
+       ((equal? (car neo-code) 'bool)
+        (if (equal? (length neo-code) 3)
+            (list 'bool-exp (cadr neo-code) (neo-parser (caddr neo-code)) '())
+        (cons 'bool-exp (cons (cadr neo-code) (map neo-parser (cddr neo-code))))))
       ;(math op num1 num2) > (math-exp op (neo-exp) (neo-exp))
       ((equal? (car neo-code) 'math)
        (list 'math-exp (cadr neo-code)
              (neo-parser (caddr neo-code))
              (neo-parser (cadddr neo-code))))
+      ;(ask (bool op num1 num2) (neo-exp1) (neo-exp2)) > (ask-exp (bool-exp ...) (parsed-neo-exp1) (parsed-neo-exp2))
+      ((equal? (car neo-code) 'ask)
+       (cons 'ask-exp
+             (map neo-parser (cdr neo-code))))
       ;(function (x y z,...) x)
       ((equal? (car neo-code) 'function)
        (list 'func-exp
@@ -61,15 +71,25 @@
 (define run-neo-parsed-code
   (lambda (parsed-code env)
     (cond
+      ((null? parsed-code) '())
       ((equal? (car parsed-code) 'num-exp)
        (cadr parsed-code));(num-exp 22)
       ((equal? (car parsed-code) 'var-exp)
        (resolve env (cadr parsed-code)))
+      ;(bool-exp op (neo-exp) (neo-exp))
+      ((equal? (car parsed-code) 'bool-exp)
+       (run-bool-exp (cadr parsed-code)
+                     (run-neo-parsed-code (caddr parsed-code) env)
+                     (run-neo-parsed-code (cadddr parsed-code) env)))
       ;(math-exp op (neo-exp) (neo-exp))
       ((equal? (car parsed-code) 'math-exp)
        (run-math-exp (cadr parsed-code)
                      (run-neo-parsed-code (caddr parsed-code) env)
                      (run-neo-parsed-code (cadddr parsed-code) env)))
+      ((equal? (car parsed-code) 'ask-exp)
+       (if (run-neo-parsed-code (cadr parsed-code) env)
+           (run-neo-parsed-code (caddr parsed-code) env)
+           (run-neo-parsed-code (cadddr parsed-code) env)))
       ((equal? (car parsed-code) 'func-exp)
        (run-neo-parsed-code (cadr (caddr parsed-code)) env))
       (else (run-neo-parsed-code
@@ -82,6 +102,23 @@
             )
       )
     ) 
+  )
+
+;tool to understand bool-exp
+(define run-bool-exp
+  (lambda (op num1 num2)
+    (cond
+      ((equal? op '>) (> num1 num2))
+      ((equal? op '<) (< num1 num2))
+      ((equal? op '>=) (>= num1 num2))
+      ((equal? op '<=) (<= num1 num2))
+      ((equal? op '==) (= num1 num2))
+      ((equal? op '!=) (not (= num1 num2)))
+      ((equal? op '&&) (and num1 num2))
+      ((equal? op '||) (or num1 num2))
+      (else (not num1))
+      )
+    )
   )
 
 ;tool to deal with math
@@ -101,7 +138,7 @@
 
 ;(define env '((a 1) (b 2) (c 5)))
 ;(app-exp (func-exp (params (identifier1, identifier2, identifer3 ...)) (body-exp)) ((neo-exp1 neo-exp2 neo-exp3 ...))
-(define sample-code '(call (function () (math + (math + a b) c)) ()))
-;(display (neo-parser sample-code))
+(define sample-code '(call (function () (ask (bool > a b) (math - a b) (math + a b))) (a)))
+(display (neo-parser sample-code))
 (define parsed-neo-code (neo-parser sample-code))
 (run-neo-parsed-code parsed-neo-code env)
